a55c23dd47bbd51d2ccf5fe5ffe895a65273162c
Kaan <49414441+kaankutucu@users.noreply.github.com>
2022-03-01 13:23:09 +0300

    PropTypes and isRequired added

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,19 @@
+import PropTypes from "prop-types";
+
 function User ({name, surname, isLoggedIn, age}) {
     return <h1>{isLoggedIn ? `${name} ${surname} ${age}` : "Giriş Yapmadınız."}</h1>;
 }
+
+
+User.prototype = {
+    name: PropTypes.string.isRequired,
+    surname: PropTypes.string,
+    isLoggedIn: PropTypes.bool,
+    age: PropTypes.number,
+
+
+};
+
 
 export default User;
 
--- src/components/friend.js
+++ src/components/friend.js
@@ -1,14 +1,21 @@
-function Friend ()  {
+import PropTypes from "prop-types";
+
+
+function Friend() {
     return (
         <>
-        <h1>{["Kaan2", "Kaan3", "Kaan4"]}</h1>;
+            <h1>{["Kaan2", "Kaan3", "Kaan4"]}</h1>;
 
             {
-                Friend.map ((Friend) => (
+                Friend.map((Friend) => (
                     <div>{Friend}</div>
                 ))}
         </>
     )
+}
+
+Friend.prototype = {
+    Friend: PropTypes.array.isRequired,
 }
 
 export default Friend;
